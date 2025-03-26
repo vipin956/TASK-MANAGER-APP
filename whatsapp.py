0cc52f529ef168787409c893004faeb8ae30d2de
vipin956 <168722955+vipin956@users.noreply.github.com>
2025-03-26 11:54:42 +0530

    Delete whatsapp.py

--- whatsapp.py
+++
@@ -1,9 +0,0 @@
-import pywhatkit as pwk
-
-phone_number = '+919876543210'
-message = 'this is a automation message'
-
-time_hour = 11  #hours
-time_min = 31   #minutes
-
-pwk.sendwhatmsg(phone_number,message,time_hour,time_min)
